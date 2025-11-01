f915e3a1c74d7abbe94cf725c194b00830a5f7df
Satyamzz <satyamsingh18177@gmail.com>
2025-11-01 22:52:04 +0530

    edited notebook

--- notebook/test.py
+++ notebook/test.py
@@ -1,4 +1,13 @@
 from ultralytics import YOLO
-
+import cv2
 model=YOLO('../runs/detect/train16/weights/best.pt')
-results=model(source=0,show=True,conf=0.6,save=True)
+path="../objects_classroom/data/images/test/pen196.jpg"
+results=model(source=0,show=True,conf=0.6)
+
+
+# annotated_img = results[0].plot()  # Get annotated frame (NumPy array)
+# cv2.imshow("Detection Result", annotated_img)
+#
+# print("\nPress any key to close the window...")
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
